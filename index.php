ade76a0c846b9d35e1dba72a79a624a79402a5df
fadrian06 <franyeradriansanchez@gmail.com>
2024-04-05 17:27:58 -0400

    1.2. Basic PHP Syntax - PHP 8 Tutorial - Part 4

--- index.php
+++ index.php
@@ -6,6 +6,23 @@
     <?= 'Hello World' ?>
   </h1>
   <p>My first paragraph.</p>
+  <?php
+  // Comment 1
+  # Comment 2
+  /**
+   * Docblock
+   */
+  /*
+    Multi
+    line
+   */
+
+  $x = 10;
+  $y = 5;
+
+  echo "<p>$x, $y</p>";
+  # comment
+  ?> Hello
 </body>
 
 </html>
