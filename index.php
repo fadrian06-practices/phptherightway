e7e302043ceb9ef9380435b9bcc4474de22fc076
fadrian06 <franyeradriansanchez@gmail.com>
2024-04-05 20:53:57 -0400

    1.3. What Are Constants & Variable Variables In PHP - Full PHP 8 Tutorial

--- index.php
+++ index.php
@@ -1,28 +1,48 @@
-<!DOCTYPE html>
-<html>
+<?php
 
-<body>
-  <h1>
-    <?= 'Hello World' ?>
-  </h1>
-  <p>My first paragraph.</p>
-  <?php
-  // Comment 1
-  # Comment 2
-  /**
-   * Docblock
-   */
-  /*
-    Multi
-    line
-   */
+/*----------  Override variables  ----------*/
+// $firstName = 'Gio';
 
-  $x = 10;
-  $y = 5;
+// $firstName = 'Joe';
 
-  echo "<p>$x, $y</p>";
-  # comment
-  ?> Hello
-</body>
+// echo $firstName; # prints: Joe
 
-</html>
+/*----------  define()  ----------*/
+// define('STATUS_PAID', 'paid');
+
+// define('STATUS_PAID', 1); # PHP Warning
+
+// echo STATUS_PAID; # prints: paid
+// echo defined('STATUS_PAID'); # prints: 1
+
+/*----------  const  ----------*/
+// const STATUS_PAID = 'paid';
+
+// echo STATUS_PAID; # prints: paid
+
+/*----------  Differences  ----------*/
+// if (true) {
+//   // const FOO = 'bar'; # Parse error
+//   define('STATUS_PAID', 9); # works
+// }
+
+/*----------  Dynamic constants  ----------*/
+// $paid = 'PAID';
+// define("STATUS_$paid", $paid);
+
+// echo STATUS_PAID; # prints: PAID
+
+/*----------  Builtin constants  ----------*/
+// echo PHP_VERSION; # prints for example: 8.2.17
+// echo __LINE__; # prints: 37
+// echo __LINE__; # prints: 38
+// echo __FILE__; # prints for example: C:\www\index.php
+
+/*----------  Variables variables  ----------*/
+$foo = 'bar';
+$$foo = 'baz';
+
+// echo $foo, ' ', $$foo; # prints: bar baz
+// echo "$foo $$foo"; # prints: bar $bar
+echo "$foo {$$foo}"; # prints: bar baz
+// echo "$foo ${$foo}"; # prints: bar baz
